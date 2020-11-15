736b9725de5d708e7ee2202adae0f794a7915faa
Youmna <youmna.zakareya@cegedim.com>
2020-11-15 09:34:00 +0200

    Json file repository POC

--- src/main/java/BasePackage/BasePage.java
+++ src/main/java/BasePackage/BasePage.java
@@ -3,13 +3,17 @@ package BasePackage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
-
+import com.jayway.jsonpath.JsonPath;
+import org.openqa.selenium.By;
 import static BasePackage.BaseTest.driver;
 
 
 public class BasePage {
+    static File jsonFile;
 
     public BasePage(){
         PageFactory.initElements(driver,this);
@@ -57,5 +61,53 @@ public class BasePage {
             System.out.println("some exception occurred while getting the title " + title);
         }
         return title;
+    }
+
+    public static WebElement getObjectLocator(String jsonpath)
+    {
+
+        String filename = System.getProperty("user.dir") + "/src/test/resources/ObjRepo.json";
+        jsonFile = new File(filename);
+        String locatorProperty = null;
+
+        try {
+            locatorProperty = JsonPath.read(jsonFile, jsonpath).toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String locatorType = locatorProperty.split(";")[0];
+        String locatorValue = locatorProperty.split(";")[1];
+
+        WebElement Element = null;
+        switch(locatorType)
+        {
+            case "id":
+                Element = driver.findElement(By.id(locatorValue));
+                break;
+            case "name":
+                Element = driver.findElement(By.name(locatorValue));
+                break;
+
+            case "class":
+                Element = driver.findElement(By.className(locatorValue));
+                break;
+            case "css":
+                Element = driver.findElement(By.cssSelector(locatorValue));
+                break;
+            case "link":
+                Element = driver.findElement(By.linkText(locatorValue));
+                break;
+            case "partial":
+                Element = driver.findElement(By.partialLinkText(locatorValue));
+                break;
+            case "tag":
+                Element = driver.findElement(By.tagName(locatorValue));
+                break;
+            case "xpath":
+                Element = driver.findElement(By.xpath(locatorValue));
+                break;
+        }
+        return Element;
     }
 }
