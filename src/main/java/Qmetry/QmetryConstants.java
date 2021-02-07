8a9731dc0ab33f35ee8b0ad9e61daf3c2b419c59
ebrahim.mostafa <ebrahim.mostafa.1993@gmail.com>
2021-02-08 01:11:03 +0200

    Bug Fixing

--- src/main/java/Qmetry/QmetryConstants.java
+++ src/main/java/Qmetry/QmetryConstants.java
@@ -7,28 +7,28 @@ import static Utilities.LoadProperties.loadProperties;
 
 public class QmetryConstants {
 
-    public static final String INTEGRATION_EANBLED = "automation.Qmetry.enabled";
-    public static final String API_TYPE = "automation.Qmetry.apitype";
-    public static final String API_KEY = "automation.Qmetry.apikey";
-    public static final String API_URL = "automation.Qmetry.url";
-    public static final String FILE_PATH = "automation.Qmetry.filepath";
+    public static final String INTEGRATION_EANBLED = "automation.qmetry.enabled";
+    public static final String API_TYPE = "automation.qmetry.apitype";
+    public static final String API_KEY = "automation.qmetry.apikey";
+    public static final String API_URL = "automation.qmetry.url";
+    public static final String FILE_PATH = "automation.qmetry.filepath";
 //    public static String FILE_PATH = new File("/target/surefire-reports/testng-results.xml").getAbsolutePath();
-    public static final String AUTHORIZATION = "automation.Qmetry.authorization";
-    public static final String USERNAME = "automation.Qmetry.username";
-    public static final String PASSWORD = "automation.Qmetry.password";
-    public static final String TEST_RUN_NAME = "automation.Qmetry.testrunname";
-    public static final String LABELS = "automation.Qmetry.labels";
-    public static final String COMPONENTS = "automation.Qmetry.components";
-    public static final String VERSION = "automation.Qmetry.version";
-    public static final String SPRINT = "automation.Qmetry.sprint";
-    public static final String PLATFORM = "automation.Qmetry.platform";
-    public static final String COMMENT = "automation.Qmetry.comment";
-    public static final String TEST_RUN_KEY = "automation.Qmetry.testrunkey";
-    public static final String TEST_ASSET_HIERARCHY = "automation.Qmetry.testassethierarchy";
-    public static final String JIRA_FIELDS = "automation.Qmetry.jirafields";
-    public static final String DEBUG = "automation.Qmetry.debug";
-    public static final String TESTCASE_UPDATE_LEVEL = "automation.Qmetry.testcaseupdatelevel";
-    public static final String ATTACHE_FILE = "automation.Qmetry.attachefile";
+    public static final String AUTHORIZATION = "automation.qmetry.authorization";
+    public static final String USERNAME = "automation.qmetry.username";
+    public static final String PASSWORD = "automation.qmetry.password";
+    public static final String TEST_RUN_NAME = "automation.qmetry.testrunname";
+    public static final String LABELS = "automation.qmetry.labels";
+    public static final String COMPONENTS = "automation.qmetry.components";
+    public static final String VERSION = "automation.qmetry.version";
+    public static final String SPRINT = "automation.qmetry.sprint";
+    public static final String PLATFORM = "automation.qmetry.platform";
+    public static final String COMMENT = "automation.qmetry.comment";
+    public static final String TEST_RUN_KEY = "automation.qmetry.testrunkey";
+    public static final String TEST_ASSET_HIERARCHY = "automation.qmetry.testassethierarchy";
+    public static final String JIRA_FIELDS = "automation.qmetry.jirafields";
+    public static final String DEBUG = "automation.qmetry.debug";
+    public static final String TESTCASE_UPDATE_LEVEL = "automation.qmetry.testcaseupdatelevel";
+    public static final String ATTACHE_FILE = "automation.qmetry.attachefile";
 //    public static Properties p;
 public static Properties p =
         loadProperties(System.getProperty("user.dir") +
