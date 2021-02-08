8a8a3607325f0a51ee61c9f010b2076768427e38
ebrahim.mostafa <ebrahim.mostafa.1993@gmail.com>
2021-02-08 12:29:49 +0200

    cucumber integration

--- src/test/java/cucumber/runner/TestRunner.java
+++ src/test/java/cucumber/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(monochrome = true,
-        features = {"src/test/java/cucumber/features/NMSSunnyDay.feature"},
+        features = {"src/test/java/cucumber/features/Login.feature"},
         glue = {"cucumber.steps"},
         plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"}, publish = true)
 public class TestRunner extends BaseTest {
